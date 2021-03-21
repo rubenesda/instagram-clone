19b235b9d9cb8064e17108ff3846e8d38b4b60d0
Ruben Escandon G <rescandon@vovsolutions.com>
2021-03-21 00:20:51 -0500

    #4 Installed and set up ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Hello! It's instagram clone</h1>
+      <h1>Hello! Its instagram clone</h1>
     </div>
   );
 }
