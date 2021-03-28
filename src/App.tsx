db14b1a2ca571ec9eff0277a779f5c944ec8d27f
Ruben Escandon G <rescandon@vovsolutions.com>
2021-03-27 20:06:53 -0500

    #8 Created routes file and prepared Login Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './Constants/routes';
+
+const Login = lazy(() => import('./Pages/Login'));
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello! Its instagram clone</h1>
-    </div>
+    <Router>
+      <Suspense fallback={<p>It is loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
